ad95237421edbe9aef3d1d730087a0d992cfaf51
humbertovnavarro <humberto.navarro.dev@gmail.com>
2022-11-22 20:08:25 -0800

    wipe

--- src/templates/whitelist-error-offensive-name.ts
+++
@@ -1,4 +0,0 @@
-export default `
-Your create application was denied for having an offensive discord and or minecraft name.
- Please change your name, rejoin and try again.
-`.trim();
--- src/templates/whitelist-error.ts
+++
@@ -1,5 +0,0 @@
-export default `
-I tried to whitelist your account but something went wrong,
- this might be because the server is undergoing maintenance, or we are in
- between seasons. Check the annoucements channel for more information.
-`.trim();
